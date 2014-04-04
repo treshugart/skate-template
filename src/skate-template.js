95feef9492238e7a91849cd8e83f1e34f593c595
Trey Shugart <treshugart@gmail.com>
2014-04-04 11:34:35 +1100

    Async compilation and rendering. Hooks for multiple templating languages.

--- src/skate-template.js
+++ src/skate-template.js
@@ -1,39 +1,67 @@
-(function() {
+(function () {
+  'use strict';
+
   var loaded = {};
   var config = {
-    attribute: 'data-template',
+    compile: function (source, done) {
+      done(source);
+    },
+    render: function (element, template, context) {
+      element.innerHTML = template(context);
+    },
+    destAttr: 'data-template-name',
+    domProperty: 'template',
     prefix: 'templates/',
+    selector: '[data-template]',
+    srcAttr: 'data-template',
     suffix: '.html'
   };
 
-  skate('[' + config.attribute + ']', {
-    ready: function(done) {
-      var that = this;
-      var templateName = this.getAttribute(config.attribute);
-      var templateNode;
-
-      if (loaded[templateName]) {
-        return render(loaded[templateName]);
+  skate(config.selector, {
+    ready: function (done) {
+      function loadUsingAjax () {
+        var request = new XMLHttpRequest();
+        request.open('GET', config.prefix + templateName + config.suffix, true);
+        request.onreadystatechange = function () {
+          if (request.readyState === 4 && request.status === 200) {
+            return compileAndRender(request.responseText, context);
+          }
+        };
+        request.send();
       }
 
-      if (templateNode = document.getElementById(templateName)) {
-        return render(templateNode.innerHTML);
+      function compile (src, done) {
+        config.compile(src, function (compiled) {
+          done(loaded[templateName] = compiled);
+        });
       }
 
-      request = new XMLHttpRequest;
-      request.open('GET', config.prefix + templateName + config.suffix, true);
-      request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
-          render(request.responseText);
-        }
-      };
-      request.send();
+      function compileAndRender (src, ctx) {
+        compile(src, function () {
+          render(ctx);
+        });
+      }
 
-
-      function render(data) {
-        that.innerHTML = loaded[templateName] = data;
+      function render (ctx) {
+        that[config.domProperty] = config.render(that, loaded[templateName], ctx);
         done();
       }
+
+      var that = this;
+      var context = {};
+      var templateName = this.getAttribute(config.srcAttr);
+
+      if (loaded[templateName]) {
+        return render(context);
+      }
+
+      var templateNode = document.querySelector('[' + config.destAttr + '="' + templateName + '"]');
+
+      if (templateNode) {
+        return compileAndRender(templateNode.innerHTML, context);
+      }
+
+      return loadUsingAjax();
     }
   });
 }());
